34261378b69b1c88555a1800154dcac0143a68b8
Rose Canoy <rose369canoy@gmail.com>
2022-03-27 19:46:12 +0800

    firstCommit

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewpoint" content="width=deivce-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie-edge">
+    <title>Open Weather API</title>
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300|Sonsie+One" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="css/style.css">
+
+  </head>
+
+  <body>
+    <header>
+      <h1>JAPAN FORECAST</h1>
+    </header>
+
+
+    <footer>
+      <p>©Copyright 2050 by nobody. All rights reversed.</p>
+    </footer>
+
+  </body>
+</html>
